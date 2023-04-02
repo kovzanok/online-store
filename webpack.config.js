0d5897376743db71e32e18ad9e93edd9e2101ce9
kovzanok <alexkovzanok@gmail.com>
2023-04-02 13:45:02 +0300

    feat: add webpack, ts and eslint configs to project

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const config = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    assetModuleFilename: "assets/[name][ext]",
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Online Store",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+};
+
+module.exports = (mode) => {
+  if (mode === "development") {
+    config.mode = "development";
+    config.devtool = "inline-source-map";
+    config.devServer = {
+      hot: true,
+    };
+  } else {
+    config.mode = "production ";
+    config.devtool = false;
+    /*config.plugins.push(
+      new ESLintPlugin({
+        extensions: ["ts", "js"],
+      })
+    );*/
+  }
+
+  return config;
+};
