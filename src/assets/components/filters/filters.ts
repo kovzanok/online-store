86baa4509739840af9353d5d2e18ca2a2dc2d10c
kovzanok <alexkovzanok@gmail.com>
2023-04-06 22:08:30 +0300

    feat: add separate span for filter count

--- src/assets/components/filters/filters.ts
+++ src/assets/components/filters/filters.ts
@@ -3,17 +3,19 @@ import { capitalize } from "../../utilities/utilities";
 
 export class Filter implements IFilter {
   gamesArray: Array<game>;
-  filterName:filterCriteria;
-  constructor(gamesArray: Array<game>,filterName:filterCriteria) {
-    this.gamesArray=gamesArray;
-    this.filterName=filterName;
+  filterName: filterCriteria;
+  constructor(gamesArray: Array<game>, filterName: filterCriteria) {
+    this.gamesArray = gamesArray;
+    this.filterName = filterName;
   }
 
-  generateFilterArray():Array<filter> {
+  generateFilterArray(): Array<filter> {
     const filtersArray: Array<filter> = [];
 
     this.gamesArray.forEach((currentGame) => {
-      const currentFilter=<filterCriteria>currentGame[this.filterName as keyof game];      
+      const currentFilter = <filterCriteria>(
+        currentGame[this.filterName as keyof game]
+      );
       if (!filtersArray.find((filter) => filter.filterName === currentFilter)) {
         const filterCount = this.gamesArray.filter(
           (game) => game[this.filterName as keyof game] === currentFilter
@@ -29,28 +31,31 @@ export class Filter implements IFilter {
     return filtersArray;
   }
 
-  renderFilter():HTMLDivElement {
-    const container:HTMLDivElement=document.createElement('div');
-    container.className=`filters__${this.filterName} ${this.filterName}`;
+  renderFilter(): HTMLDivElement {
+    const container: HTMLDivElement = document.createElement("div");
+    container.className = `filters__${this.filterName} ${this.filterName}`;
 
-    const title:HTMLHeadingElement=document.createElement('h1');
-    title.className=`${this.filterName}__title filters__title`;
-    title.textContent=capitalize(this.filterName);
+    const title: HTMLHeadingElement = document.createElement("h1");
+    title.className = `${this.filterName}__title filters__title`;
+    title.textContent = capitalize(this.filterName);
 
-    const filtersArray=this.generateFilterArray();
-    const list=this.renderFilterList(filtersArray);
+    const filtersArray = this.generateFilterArray();
+    const list = this.renderFilterList(filtersArray);
 
-    container.append(title,list);
+    container.append(title, list);
 
     return container;
   }
 
-  renderFilterList(filtersArray: Array<filter>):HTMLUListElement {
+  renderFilterList(filtersArray: Array<filter>): HTMLUListElement {
     const list: HTMLUListElement = document.createElement("ul");
     list.className = `${this.filterName}__list filters__list`;
 
-    filtersArray.forEach((currentGenre) => {      
-      const item=this.renderFilterItem(currentGenre.filterName, currentGenre.count);
+    filtersArray.forEach((currentGenre) => {
+      const item = this.renderFilterItem(
+        currentGenre.filterName,
+        currentGenre.count
+      );
       list.append(item);
     });
 
@@ -61,7 +66,7 @@ export class Filter implements IFilter {
     filteredName: string,
     totalCount: number,
     activeCount: number = totalCount
-  ):HTMLLIElement {
+  ): HTMLLIElement {
     const filteredNameID = filteredName.toLowerCase();
     const item: HTMLLIElement = document.createElement("li");
     item.className = `${this.filterName}__item filters__item`;
@@ -72,13 +77,21 @@ export class Filter implements IFilter {
 
     const label: HTMLLabelElement = document.createElement("label");
     label.setAttribute("for", filteredNameID);
-    label.textContent=filteredName;
+    label.textContent = filteredName;
 
-    const genreCount: HTMLSpanElement = document.createElement("span");
-    genreCount.className = `${this.filterName}__count filters__count`;
-    genreCount.textContent = `(${activeCount}/${totalCount})`;
+    const filterCount: HTMLSpanElement = document.createElement("span");
+    filterCount.className = `${this.filterName}__count filters__count`;
 
-    item.append(input, label, genreCount);
+    const filterActive: HTMLSpanElement = document.createElement("span");
+    filterActive.className='count__current';
+    filterActive.textContent=String(activeCount);
+
+    const filterTotal: HTMLSpanElement = document.createElement("span");
+    filterTotal.className='count__total';
+    filterTotal.textContent=String(totalCount);
+    
+    filterCount.append('(',filterActive,'/',filterTotal,')');
+    item.append(input, label, filterCount);
     return item;
   }
 }
