f5e94e8eed7fce650d13bf74388a8ec34fd2530d
kovzanok <alexkovzanok@gmail.com>
2023-04-20 12:04:56 +0300

    refactor: remove all project from main to dev branch

--- src/assets/components/not-found-page/NotFound.ts
+++
@@ -1,9 +0,0 @@
-export class NotFoundPage {
-    renderPage() {
-        const notFoundPage=document.createElement('div');
-        notFoundPage.className='not-found-page';
-        notFoundPage.textContent='Page not found (404)';
-
-        return notFoundPage;
-    }
-}
