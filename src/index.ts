bcb0687cba9f9c87b925538e7faa1bd572a916c8
kovzanok <alexkovzanok@gmail.com>
2023-04-21 20:56:08 +0300

    fix: fix not responding header element

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
+import { Header } from './assets/components/header/Header';
 import { Router } from './assets/components/router/Router';
 import { Game } from './assets/types';
 // tslint:disable-next-line
 require('./styles.scss');
 
+const header = new Header();
+header.start();
+
 const games: Array<Game> = require('./assets/json/games.json');
 const router = new Router(games);
 router.start();
+
 
 
 
